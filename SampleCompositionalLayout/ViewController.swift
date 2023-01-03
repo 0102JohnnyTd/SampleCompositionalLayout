5d505bbfe0eb076fedd85ed373f8fa5ac5862a48
0102johnny <rt2824.recruit.1022@gmail.com>
2023-01-03 13:45:35 +0900

    Initial commit

---
+++ SampleCompositionalLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleCompositionalLayout
+//
+//  Created by Johnny Toda on 2023/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
